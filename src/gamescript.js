c0a0b27cbeeb8e70bda6b1f45eb8a210541d2c9e
anderson <anderson.allan.sobral@gmail.com>
2016-12-24 17:41:02 -0300

    demo terminada

--- src/gamescript.js
+++ src/gamescript.js
@@ -1,9 +1,17 @@
 var gameArray = [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7];
 var pickedTiles = [];
+var scoreText;
+var moves=0;
 var gameLayer ={};
+var shuffle = function(v){
+    for(var j, x, i = v.length; i; j = parseInt(Math.random() * i),x = v[--i], v[i] = v[j], v[j] = x);
+        return v;
+};
+
 var gameScene = cc.Scene.extend({
  onEnter:function () {
      this._super();
+     gameArray = shuffle(gameArray);
      gameLayer = new game();
      gameLayer.init();
      this.addChild(gameLayer);
@@ -15,6 +23,9 @@ var game = cc.Layer.extend({
      var gradient = cc.LayerGradient.create(cc.color(0,0,0,255),
      cc.color(0x46,0x82,0xB4,255));
      this.addChild(gradient);
+     scoreText = cc.LabelTTF.create("Moves: 0","Arial","32",cc.TEXT_ALIGNMENT_CENTER);
+     this.addChild(scoreText);
+     scoreText.setPosition(90,50);
      for(i=0;i<16;i++){
          var tile = new MemoryTile();
          tile.pictureValue = gameArray[i];
@@ -47,18 +58,19 @@ var listener = cc.EventListener.create({
 });
 
 function checkTiles(){
- 
+ moves++;
+ scoreText.setString("Moves: "+moves);
  var pause = setTimeout(function(){
- if(pickedTiles[0].pictureValue!=pickedTiles[1].pictureValue){
- pickedTiles[0].initWithFile("src/assets/cover.png");
- pickedTiles[1].initWithFile("src/assets/cover.png");
- }
- else{
- gameLayer.removeChild(pickedTiles[0]);
- gameLayer.removeChild(pickedTiles[1]);
- }
- pickedTiles = [];
- },500);
+     if(pickedTiles[0].pictureValue!=pickedTiles[1].pictureValue){
+         pickedTiles[0].initWithFile("src/assets/cover.png");
+         pickedTiles[1].initWithFile("src/assets/cover.png");
+     }
+     else{
+         gameLayer.removeChild(pickedTiles[0]);
+         gameLayer.removeChild(pickedTiles[1]);
+     }
+     pickedTiles = [];
+     },500);
 }
 
 var MemoryTile = cc.Sprite.extend({
