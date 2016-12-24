a365c81e59c55019c5ff32a9dc74598737f6d260
anderson <anderson.allan.sobral@gmail.com>
2016-12-24 16:55:15 -0300

    acrescentando eliminação das imagens após comparação correta do jogador

--- src/loadassets.js
+++ src/loadassets.js
@@ -1,3 +1,10 @@
-var gameResources = [
- "src/assets/target.png"
+var gameResources = ["src/assets/cover.png",
+ "src/assets/tile_0.png",
+ "src/assets/tile_1.png",
+ "src/assets/tile_2.png",
+ "src/assets/tile_3.png",
+ "src/assets/tile_4.png",
+ "src/assets/tile_5.png",
+ "src/assets/tile_6.png",
+ "src/assets/tile_7.png"
 ];
